e63643837ed4a012cf8ac2d44baf600e347c0311
23c-nyeki-dotcom <23c-nyeki@ipari.vein.hu>
2026-03-11 11:17:57 +0100

    id lekeredezes js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const try = ()=>{
-
-}
+let valasz1 = document.getElementById("valasz1")
+let valasz2 = document.getElementById("valasz2")
+let valasz3 = document.getElementById("valasz3")
+let valasz4 = document.getElementById("valasz4")
+let valasz5 = document.getElementById("valasz5")
+let valasz6 = document.getElementById("valasz6")
+let valasz7 = document.getElementById("valasz7")
+let valasz8 = document.getElementById("valasz8")
+let valasz9 = document.getElementById("valasz9")
+let valasz10 = document.getElementById("valasz10")
 
 function ellenorzes() {
-
+    
 }
 
 document.getElementById("kiertekel").addEventListener("click", ellenorzes)
