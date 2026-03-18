0c583a0da3ff9527fc8ec8e995cb4460e78ad900
Bocskay-Botond <bocsbo08@gmail.com>
2026-03-18 10:10:30 +0100

    egyes,kettes js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let valasz1 = document.getElementById("valasz1")
-let valasz2 = document.getElementById("valasz2")
+let valasz1 = document.getElementById("1.2_válasz")
+let valasz2 = document.getElementById("2.2_válasz")
 let valasz3 = document.getElementById("valasz3")
 let valasz4 = document.getElementById("valasz4")
 let valasz5 = document.getElementById("valasz5")
@@ -10,7 +10,19 @@ let valasz9 = document.getElementById("valasz9")
 let valasz10 = document.getElementById("valasz10")
 let valasz11 = document.getElementById("valasz+1")
 
-function ellenorzes() {
+function ellenorzes_kerdes_1() {
+    if (valasz1.checked) {
+        return 1;
+    }
+    return 0;
+}
+function ellenorzes_kerdes_2() {
+    if (valasz2.checked) {
+        return 1;
+    }
+    return 0;
+}
+function ellenorzes_kerdes_3(params) {
     
 }
 
